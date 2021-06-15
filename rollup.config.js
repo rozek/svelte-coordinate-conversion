292d56fea2120cbb18c7e16f44cbb126689a97ab
Andreas Rozek <A.Rozek@gmx.de>
2021-06-15 09:29:24 +0200

    change to build both ESM and UMD modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,28 @@
 // see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
 
+import commonjs   from '@rollup/plugin-commonjs'
+import resolve    from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './svelte-coordinate-conversion.ts',
-  output: {
-    dir: './',
-    format: 'umd',  // builds for both Node.js and Browser
-    name:'Conversion', // required for UMD modules
-    noConflict:true,
-    sourcemap: true,
-    exports: 'default',
-  },
-  plugins: [typescript(), terser()],
+  output: [
+    {
+      dir:       './',
+      format:    'umd', // builds for both Node.js and Browser
+      name:      'Conversion', // required for UMD modules
+      noConflict:true,
+      sourcemap: true,
+      exports: 'default'
+    },{
+      file:     './svelte-coordinate-conversion.esm.js',
+      format:   'esm',
+      sourcemap:true
+    }
+  ],
+  plugins: [
+    commonjs(), resolve(), typescript(),
+    terser({ format:{ comments:false } })
+  ],
 };
